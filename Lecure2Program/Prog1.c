a54b0aaea62f4ae134ce8cae8d1c499469c0fd76
Ryan <etrigan976@gmail.com>
2025-04-02 12:58:42 -0700

    Lecture 2

---
+++ Lecure2Program/Prog1.c
@@ -0,0 +1,123 @@
+#include  "FPToolkit.c"
+
+int main()
+{
+    double swidth, sheight;
+
+    swidth = 800.0; sheight = 800.0;
+    G_init_graphics(swidth, sheight);
+    G_rgb(1.0, 1.0, 1.0);
+    G_clear();
+    int size_num;
+    printf("Type in pos int\n");
+    scanf("%d", &size_num);
+    printf("%d", size_num); 
+    
+    for (double i = 0; i <= size_num; i += 1.0)
+    {
+        G_rgb(0.0, 0.0, 0.0);
+        G_line(swidth*(i/size_num)*1.0, 0.0, swidth*(i/size_num)*1.0, sheight*1.0);
+    }
+    for (double i = 0; i <= size_num; i += 1.0)
+    {
+        G_rgb(0.0, 0.0, 0.0);
+        G_line(0.0, sheight*(i/size_num)*1.0, swidth*1.0, sheight*(i/size_num)*1.0);
+    }
+    int key;
+    key = G_wait_key();
+}
+
+/*int main()
+{
+   int    swidth, sheight ;
+   double lowleftx, lowlefty, width, height ;
+   double x[10], y[10] ;
+   double numxy ;
+   double a[20], b[20] ;
+   double numab ;
+
+   
+   // must do this before you do 'almost' any other graphical tasks 
+   swidth = 400 ;  sheight = 600 ;
+   G_init_graphics (swidth,sheight) ;  // interactive graphics
+
+   
+   // clear the screen in a given color
+   G_rgb (0.3, 0.3, 0.3) ; // dark gray
+   G_clear () ;
+
+   
+   // draw a point
+   G_rgb (1.0, 0.0, 0.0) ; // red
+   G_point (200, 580) ; // hard to see
+
+   
+   // draw a line
+   G_rgb (0.0, 1.0, 0.0) ; // green
+   G_line (0,0, swidth-1, sheight-1) ;
+
+   
+   // aligned rectangles
+   G_rgb (0.0, 0.0, 1.0) ; // blue
+   lowleftx = 200 ; lowlefty = 50 ; width = 10 ; height = 30 ;
+   G_rectangle (lowleftx, lowlefty, width, height) ;
+   lowleftx = 250 ; 
+   G_fill_rectangle (lowleftx, lowlefty, width, height) ;
+
+   
+   // triangles
+   G_rgb (1.0, 1.0, 0.0) ; // yellow
+   G_triangle (10, 300,  40,300,  60,250) ;
+   G_fill_triangle (10,100,  40,100,  60,150) ;
+
+   
+   // circles   
+   G_rgb (1.0, 0.5, 0.0) ; // orange
+   G_circle (100, 300, 75) ;
+   G_fill_circle (370, 200, 50) ;
+
+   
+   // polygons
+   G_rgb (0.0, 0.0, 0.0) ; // black
+   x[0] = 100 ;   y[0] = 100 ;
+   x[1] = 100 ;   y[1] = 300 ;
+   x[2] = 300 ;   y[2] = 300 ;
+   x[3] = 300 ;   y[3] = 100 ;
+   x[4] = 200 ;   y[4] = 175 ;
+   numxy = 5 ;
+   G_polygon (x,y,numxy) ;
+
+
+   G_rgb (0.4, 0.2, 0.1) ; // brown
+   a[0] = 300 ;   b[0] = 400 ;
+   a[1] = 350 ;   b[1] = 450 ;
+   a[2] = 275 ;   b[2] = 500 ;
+   a[3] = 125 ;   b[3] = 400 ;
+   numab = 4 ;
+   G_fill_polygon (a,b,numab) ;
+
+
+
+   //===============================================
+   
+   double p[2], q[2] ;
+
+   G_rgb(1,0,0) ;
+   
+   G_wait_click(p) ;
+   G_fill_circle(p[0],p[1],2) ;
+
+   G_wait_click(q) ;
+   G_fill_circle(q[0],q[1],2) ;   
+
+   G_rgb(0,1,0.5) ;
+   G_line(p[0],p[1], q[0],q[1]) ;
+   
+   int key ;   
+   key =  G_wait_key() ; // pause so user can see results
+   
+   //printf("%d", &key);
+
+   //G_save_image_to_file("demo.xwd") ;
+   G_save_to_bmp_file("demo.bmp") ;
+}*/
