a2f984c37c928261913cac9afb46c30a05a429e6
Ryan <etrigan976@gmail.com>
2025-06-06 20:22:24 -0700

    PUSH

--- PortfolioComp/Pythagoras_tree.py
+++ PortfolioComp/Pythagoras_tree.py
@@ -2,27 +2,34 @@ from Gkit import *
 from ctypes import *
 from math import *
 
-from random import *
-
 def main():
     swidth : float = 800 
     sheight : float = 800
     depth_val : int = 0
-    #printf("Please enter the depth of the Sierpinski Triangle: ");
-    #if (scanf("%d", &depth_val) != 1 || depth_val < 0) {
-    #    printf("Invalid input. Depth must be a positive integer.\n");
-    #    return 0;
-    #}
     depth_val = int(input("Please enter the depth of the Sierpinski Triangle: "))
     if depth_val < 0: return
     G_init_graphics(swidth, sheight)
     G_rgb(0.0, 0.0, 0.0)
     G_clear()
-    for i in range(depth_val, -1, -1):
-        color_r : float = random()
-        color_g : float = random()
-        color_b : float = random()
-        pythagoras_tree(400, 100, 100, 0, color_r, color_g, color_b, i)
+    G_rgb(150 / 255, 75 / 255, 0 / 255)
+    G_fill_rectangle(0, 0, swidth, 100)
+    G_rgb(135 / 255, 206 / 255, 235 / 255)
+    G_fill_rectangle(0, 100, swidth, sheight - 100)
+    G_rgb(1, 1, 0)
+    G_fill_circle(100, sheight - 100, 50)
+    num_trees = depth_val + 1
+    base_size = min(100, swidth / (num_trees * 1.5))
+    spacing = swidth / num_trees
+
+    for i in range(num_trees):
+        x = (i + 0.5) * spacing
+        y = 100
+
+        color_r = 0.0
+        color_g = i / max(num_trees - 1, 1)
+        color_b = 0.0
+        tree_offset = base_size * 0.5
+        pythagoras_tree(x - tree_offset-100, y, base_size, 0, color_r, color_g, color_b, i)
         G_wait_key()
     G_wait_key()
     G_save_to_bmp_file("pythag.bmp")
